e95c6ee7fa851ba3cdb1a851772d124a6ee2639c
AlanSoto <ing.alansoto@gmail.com>
2026-03-16 13:45:31 -0500

    Initialization of entities and environment package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TouristsModule } from './tourists/tourists.module';
+import { ProvidersModule } from './providers/providers.module';
+import { ExperiencesModule } from './experiences/experiences.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { FinanceModule } from './finance/finance.module';
 
 @Module({
-  imports: [],
+  imports: [
+
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TouristsModule, 
+    ProvidersModule, 
+    ExperiencesModule, 
+    BookingsModule, 
+    FinanceModule],
   controllers: [AppController],
   providers: [AppService],
 })
